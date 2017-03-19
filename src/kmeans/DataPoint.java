f9aa224d68227c9530c4117b8e8ebfb020a39047
Christian Binder <binderchri@gmail.com>
2017-03-20 00:05:46 +0100

    removed some comments

--- src/kmeans/DataPoint.java
+++ src/kmeans/DataPoint.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kmeans;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-/**
- *
- * @author binderchri
- */
 public class DataPoint {
 
     private final int dimensions;
--- src/kmeans/Generator.java
+++ src/kmeans/Generator.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kmeans;
 
 import java.io.Console;
 import java.util.Random;
 import java.util.function.Supplier;
 
-/**
- *
- * @author binderchri
- */
 public class Generator {
 
     private final int clusterCount;
